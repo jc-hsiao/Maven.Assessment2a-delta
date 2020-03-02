f99b796ef4e19083d3d92cffcf6c22170bf77c6e
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-03-02 10:01:00 -0500

    part2 collections finished

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,37 +1,48 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Use a map to keep track of inventory in a store
  */
 public class Inventory {
+    Map<String,Integer> items;
+
     /**
      * @param strings list of strings to add / remove / fetch from
      */
     public Inventory(List<String> strings) {
-
+        items = new HashMap<>();
+        for(String s :strings){
+            items.put(s,1);
+        }
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+        items = new HashMap<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        if(items.containsKey(item))
+            items.replace(item,items.get(item)+1);
+        else
+            items.put(item,1);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        items.replace(item,items.get(item)-1);
     }
 
     /**
@@ -39,6 +50,6 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        return items.getOrDefault(item, 0);
     }
 }
--- src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
+++ src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
@@ -1,5 +1,8 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Use a map to solve
  */
@@ -8,8 +11,10 @@ public class MonthConversion {
      * @param monthNumber - ordinal of month in the year; i.e. January = 1, February = 2
      * @param monthName - name of month
      */
-    public void add(Integer monthNumber, String monthName) {
 
+    Map<Integer,String> m = new HashMap<>();
+    public void add(Integer monthNumber, String monthName) {
+        m.put(monthNumber,monthName);
     }
 
     /**
@@ -17,15 +22,21 @@ public class MonthConversion {
      * @return the name of the respective month
      */
     public String getName(Integer monthNumber) {
-        throw new NullPointerException();
+        return m.get(monthNumber);
+        //throw new NullPointerException();
     }
 
     /**
      * @param monthName - name of month
      * @return - the ordinal of the month in the year
      */
-    public int getNumber(String monthName) {
-        return (Integer)null;
+    public Integer getNumber(String monthName) {
+        for (Map.Entry<Integer, String> entry : m.entrySet()) {
+            if (entry.getValue().equals(monthName)) {
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 
     /**
@@ -33,7 +44,7 @@ public class MonthConversion {
      * @return true if the monthNumber is in the keySet
      */
     public Boolean isValidNumber(Integer monthNumber) {
-        return null;
+        return m.containsKey(monthNumber);
     }
 
     /**
@@ -41,14 +52,14 @@ public class MonthConversion {
      * @return true if the monthName is in the valueSet
      */
     public Boolean isValidMonth(String monthName) {
-        return null;
+        return m.containsValue(monthName);
     }
 
     /**
      * @return number of entries in this mapping
      */
     public Integer size() {
-        return -1;
+        return m.size();
     }
 
     /**
@@ -56,6 +67,6 @@ public class MonthConversion {
      * @param monthName - name of month
      */
     public void update(Integer monthNumber, String monthName) {
-
+        m.replace(monthNumber,monthName);
     }
 }
